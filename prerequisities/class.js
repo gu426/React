faf58dafa10ac91ab4e9f597920bd2cacbf4b451
Gurmeet <84338317+gu426@users.noreply.github.com>
2022-04-13 23:30:40 +0530

    code

--- prerequisities/class.js
+++ prerequisities/class.js
@@ -26,6 +26,10 @@ class Person {
     constructor(name, age) {
       (this.name = name), (this.age = age);
     }
+
+    welcome(){
+        console.log("hello"+ this.name)
+    }
   }
   
   class Teacher extends Person {
@@ -40,12 +44,18 @@ class Person {
       super(name, age);
       this.cgpa = cgpa;
     }
+    hello(){
+        super.welcome()
+    }
   }
   
   let person1 = new Person("Adam", 25);
   let teacher1 = new Teacher("Steve", 40, 100);
   let student1 = new Student("Mark", 21, 8.0);
   
-  console.log(person1);
-  console.log(teacher1);
-  console.log(student1);
+//   console.log(person1);
+//   console.log(teacher1);
+//   console.log(student1);
+
+  person1.welcome()
+  student1.hello()
